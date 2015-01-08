3e54a8038ef7e173f6262bd355a8f45ea4be6930
Berin Martini <berin.martini@gmail.com>
2015-01-08 14:31:56 -0500

    Update read-memory application
    Now setup to read from memory what was written by the write-memory app.

--- app/read-memory.c
+++ app/read-memory.c
@@ -1,46 +1,57 @@
+#include <interface.h>
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <sys/mman.h>
+
+enum cfg_regsiters {
+	CFG_AXIS_ADDR,
+	CFG_AXIS_DATA,
+	CFG_HP0_DST_CNT,
+	CFG_HP0_SRC_CNT,
+	CFG_HP0_DST_DATA,
+	CFG_HP0_SRC_DATA,
+	CFG_EMPTY,
+};
 
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	int total = 0;
 
-	if (argc < 3) {
-		printf("Usage: %s <phys_addr> <length>\n", argv[0]);
+	if (axis_init("/dev/axis") != 0) {
+		return -1;
+	}
+
+	if (argc < 2) {
+		printf("Usage: %s <length>\n", argv[0]);
 		return 0;
 	}
 
-	//off_t offset = 462422016;
-	off_t offset = atoi(argv[1]);
-	size_t len = atoi(argv[2]);
-	printf("phys_addr: %ld, %02x, length: %ld\n", (long int)offset, (unsigned int)offset,
-	       (long int)len);
+	off_t offset = 0x1b900000;
+	size_t len = atoi(argv[1]);
+	printf("phys_addr: %ld, %02x, length: %ld\n", (long int)offset,
+	       (unsigned int)offset, (long int)len);
 
-	int fd = open("/dev/mem", O_SYNC);
-	unsigned short *mem =
-	    (unsigned short *)mmap(NULL, len, PROT_READ | PROT_WRITE,
-				   MAP_PRIVATE, fd, offset);
 
-	if (mem == NULL) {
-		printf("Can't map memory\n");
-		return -2;
-	}
+	// configure axis to write to memory
+	printf("config start\n");
+	cfg_write(CFG_AXIS_ADDR, 1);
+	cfg_write(CFG_AXIS_DATA, offset);
+	cfg_write(CFG_AXIS_DATA, len);
+	printf("config done\n");
 
-//      for (i = 0; i < len; ++i) {
-//              mem[i] = 0xDEAD;
-//      }
-
+	// send data to be written over cfg bus
+	printf("read start\n");
+	cfg_read(CFG_HP0_SRC_DATA); // prime cfg register
 	for (i = 0; i < len; ++i) {
-		if (0xDEAD == mem[i]) {
-			total++;
-			printf("%6d, %10ld, %02x, %02x\n", total, (offset + i),
-			       (unsigned int)(offset + i), (int)mem[i]);
-		}
+		printf("%d ", cfg_read(CFG_HP0_SRC_DATA));
 	}
-	printf("\n");
+	printf("\nhp0 src cnt: %d\n", cfg_read(CFG_HP0_SRC_CNT));
+
+	if (axis_exit() != 0) {
+		return -1;
+	}
+
+	printf("\ndone\n");
 
 	return 0;
 }
